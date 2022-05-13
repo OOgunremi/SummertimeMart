0ad289f511c7c856ede3afd99fc90f6a1f699ca2
Olalekan Isaac Ogunremi <OOgunremi@yahoo.com>
2022-05-12 21:03:41 -0600

    develop home page contd

--- summertimemart/components.js/HeaderBanner.jsx
+++ summertimemart/components.js/HeaderBanner.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
 const HeaderBanner = () => {
   return (
     <div className='hero-banner-container'>
       <div>
-        <p className='beats-solo'>
-          Hello
-        </p>
+        <p className='beats-solo'>HELLO SUMMERTIME MART</p>
+        <h3>BE BOLD</h3>
+        <img src='' alt='Mobilephones' className='hero-banner-image'/>
+      </div>
+      <div>
+        <Link href="/product/:id">
+          <button type='button'>Button</button>
+        </Link>
+      </div>
+      <div className='desc'>
+        <h5>Description</h5>
+        <p>Description</p>
       </div>
     </div>
   )
