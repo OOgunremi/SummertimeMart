54d10a15d31c0a2e63c9a832b06cbe03e41da3ba
Olalekan Isaac Ogunremi <OOgunremi@yahoo.com>
2022-05-12 20:32:35 -0600

    set up components, import compnonet to index.js, build css

---
+++ summertimemart/components.js/HeaderBanner.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HeaderBanner = () => {
+  return (
+    <div className='hero-banner-container'>
+      <div>
+        <p className='beats-solo'>
+          Hello
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default HeaderBanner
