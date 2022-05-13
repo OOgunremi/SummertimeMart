0e84e099f1d3d8ae6e4df0c5d6bf32fb74c3a804
Olalekan Isaac Ogunremi <OOgunremi@yahoo.com>
2022-05-13 08:23:15 -0600

    render header banner components dynamically

--- summertimemart/components.js/HeaderBanner.jsx
+++ summertimemart/components.js/HeaderBanner.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import Link from 'next/link';
-
-const HeaderBanner = () => {
+import { urlFor } from '../lib/client';
+const HeaderBanner = ({headerBanner}) => {
   return (
     <div className='hero-banner-container'>
       <div>
-        <p className='beats-solo'>HELLO SUMMERTIME MART</p>
-        <h3>BE BOLD</h3>
-        <img src='' alt='Mobilephones' className='hero-banner-image'/>
+        {/* <p className='beats-solo'>{headerBanner.smallText}</p> */}
+        <h1>{headerBanner.largeText2}</h1>
+        <h3>{headerBanner.midText}</h3>
+        <img src={urlFor(headerBanner.image)} alt='Mobilephones' className='hero-banner-image'/>
       </div>
       <div>
-        <Link href="/product/:id">
-          <button type='button'>Button</button>
+        <Link href={`/product/${headerBanner.product}`}>
+          <button type='button'>{headerBanner.buttonText}</button>
         </Link>
       </div>
       <div className='desc'>
         <h5>Description</h5>
-        <p>Description</p>
+        <p>{headerBanner.desc}</p>
       </div>
     </div>
   )
--- summertimemart/pages/index.js
+++ summertimemart/pages/index.js
@@ -1,20 +1,31 @@
-import React from 'react'
-import { Cart, Product, Layout, Navbar, Footer, FooterBanner, HeaderBanner } from '../components.js'
+import React from 'react';
+import { Cart, Product, Layout, Navbar, Footer, FooterBanner, HeaderBanner } from '../components.js';
+import { client } from '../lib/client.js'; 
 
-const Home = () => {
-  return (
-    <>
-    <HeaderBanner/>
-      <div  className='products-heading'>
-        <h2>Best Selling Mobiles</h2>
-        <p>Showing the top rated Mobiles</p>
-      </div>
-      <div className='products-container'>
-        {['product 1', 'product 2'].map((each) => each)}
-      </div>
-    <FooterBanner/>
-    </>
-  )
+const Home = ({products, banners}) => (
+
+  <>
+  <HeaderBanner headerBanner={banners.length && banners[0]}/>
+    <div  className='products-heading'>
+      <h2>Best Selling Mobiles</h2>
+      <p>Showing the top rated Mobiles</p>
+    </div>
+    <div className='products-container'>
+      {products?.map((product) => product.name)}
+    </div>
+  <FooterBanner/>
+  </>
+);
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const banners = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, banners }
+  }
 }
 
 export default Home
