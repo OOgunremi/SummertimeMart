f1563cba4f3de9fb8e999fc9d59a85c1753ed0e5
Olalekan Isaac Ogunremi <OOgunremi@yahoo.com>
2022-05-13 21:09:03 -0600

    update slug page for product details

--- summertimemart/pages/product/[slug].js
+++ summertimemart/pages/product/[slug].js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { client, urlFor } from '../../lib/client';
+import { AiFillStar, AiOutlineStar, AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai'
+import { Product } from '../../components.js';
 
 const ProductDetails = ({product, products}) => {
   const { image, name, description, price } = product;
@@ -9,9 +11,9 @@ const ProductDetails = ({product, products}) => {
         <div>
           <div className='image-container'>
             <img src={urlFor(image && image[0])
-            } height={700}/>
+            } height={500}/>
           </div>
-          <div className='small-images-container'>
+          {/* <div className='small-images-container'>
             {image?.map((item, i) => (
               <img src={urlFor(item)}
               className=''
@@ -19,12 +21,49 @@ const ProductDetails = ({product, products}) => {
               height={200}
               />
             ))}
-          </div>
-          <div className='product-details-desc'>
+          </div> */}
+          <div className='product-detail-desc'>           
+              <h1>{name}</h1>
+              <div className='reviews'>
+                <div>
+                  <AiFillStar />
+                  <AiFillStar />
+                  <AiFillStar />
+                  <AiFillStar />
+                  <AiOutlineStar />
+                </div>
+                <p>(20)</p>
+              </div>
+              <h4>Details</h4>
+              <p>{description}</p>
+              <p className='price'> ${price} </p>
+              <div className='quantity'>
 
+              <h3>Quantity</h3>
+              <p className='quantity-desc'>
+                <span className='minus' onClick=''><AiOutlineMinus/></span>
+                <span className='num' onClick=''>2</span>
+                <span className='plus' onClick=''><AiOutlinePlus/></span>
+              </p>
+              </div>
+              <div className='buttons'>
+                <button className='add-to-cart' type= 'button' onClick=''> Add to Cart</button>
+                <button className='buy-now' type= 'button' onClick=''> Buy Now</button>
+              </div>
           </div>
         </div>
+
       </div>
+      <div className='maylike-products-wrapper'>
+          <h2>You may also like</h2>
+          <div className='marquee'>
+            <div className='maylike-products-container track'>
+              {products.map((item) => (<Product key={item._id} product={item}
+              />
+              ))}
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
@@ -62,7 +101,4 @@ export const getStaticProps = async ({params:{ slug }}) => {
     props: { products, product }
   }
 }
-
-
-
 export default ProductDetails
